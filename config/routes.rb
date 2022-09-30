dd9e1f30609d88a345578ac2152e49ab5c049335
atpking <atpking@gmail.com>
2022-09-30 11:45:00 +0800

    add sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
+
   resources :tunnels
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
