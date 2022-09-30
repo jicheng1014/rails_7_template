a85d27181c2ab0596169fee45261d8080d7a71cc
atpking <atpking@gmail.com>
2022-09-30 23:52:31 +0800

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
+  get "home/index"
   authenticate :user, ->(user) { user.admin? } do
     mount Sidekiq::Web => "/sidekiq"
   end
@@ -10,5 +11,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
